fdf2d4b4ed517f7a90805624dd1527bbed5ae272
Vojtěch Toman <cry.wolf@centrum.cz>
2024-08-28 21:59:56 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,24 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
         let input = input.trim();
 
+        if input.is_empty() {
+            continue;
+        }
+
+        let cmd = Command::parse(input);
+
+        cmd.invoke();
+    }
+}
+
+enum Command<'a> {
+    Exit(i32),
+    Echo(&'a str),
+    Unknown(&'a str),
+}
+
+impl<'a> Command<'a> {
+    pub fn parse(input: &'a str) -> Self {
         let command: &str;
         let mut args_str = "";
         let mut args: Option<Vec<&str>> = None;
@@ -23,23 +41,30 @@ fn main() {
             command = input;
         }
 
-        if command.is_empty() {
-            continue;
+        match command {
+            "exit" => {
+                let code = args
+                    .unwrap_or(vec!["0"])
+                    .first()
+                    .unwrap()
+                    .parse::<i32>()
+                    .map_err(|err| {
+                        eprintln!("Argument Error: {}", err);
+                        err
+                    })
+                    .unwrap();
+                Command::Exit(code)
+            }
+            "echo" => Command::Echo(args_str),
+            _ => Command::Unknown(input),
         }
+    }
 
-        if command == "exit" {
-            let code = if let Some(args) = args {
-                args.first().unwrap_or(&"0").parse::<i32>().unwrap()
-            } else {
-                0
-            };
-            std::process::exit(code);
+    pub fn invoke(self) {
+        match self {
+            Command::Exit(code) => std::process::exit(code),
+            Command::Echo(arg) => println!("{}", arg),
+            Command::Unknown(cmd) => println!("{}: command not found", cmd),
         }
-        if command == "echo" {
-            println!("{}", args_str);
-            continue;
-        }
-
-        println!("{}: command not found", input);
     }
 }
